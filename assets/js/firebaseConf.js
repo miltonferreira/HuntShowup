f57992cc52c139d81a328307e1ee1f7fa7899c05
miltonferreira <jotonferreira@gmail.com>
2020-12-17 15:29:31 -0300

    bug fix e analytics

---
+++ assets/js/firebaseConf.js
@@ -0,0 +1,18 @@
+function connectFirebase() {
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyCRRX9P8hjt-NUbrSXaZxZNgPwXiTXhgkc",
+        authDomain: "hunt-showup.firebaseapp.com",
+        databaseURL: "https://hunt-showup.firebaseio.com",
+        projectId: "hunt-showup",
+        storageBucket: "hunt-showup.appspot.com",
+        messagingSenderId: "485579818827",
+        appId: "1:485579818827:web:c5b99cb45f98d824ca0ace",
+        measurementId: "G-GM82QC4B8K"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+}
+
+connectFirebase();  // Inicializa credenciais para conectar ao firebase
